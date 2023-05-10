d496ca2bf257c1c61785358636fae33b1be54a86
baievv <baev.dav@gmail.com>
2023-05-10 08:23:15 +0300

    feat

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -7,7 +7,7 @@ export function useTelegram() {
   };
 
   const onToggleButton = () => {
-    if (tg.MainButton.inVisible) {
+    if (tg.MainButton.isVisible) {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
